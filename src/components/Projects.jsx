bfea96a3e08257b6528892a21c1411be5e33cab0
Yapici Esref <yapiciesref@gmail.com>
2025-06-03 03:42:05 +0300

    proje eklendi

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,16 +10,15 @@ function Projects() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 
         <div className="bg-txt-third shadow-md rounded-lg p-5 flex flex-col justify-between h-60">
-          <h2 className="text-xl font-semibold mb-4 text-bg-first">Project Name</h2>
-          <p className="text-gray-600 flex-grow">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-            vero.
+          <h2 className="text-xl font-semibold mb-4 text-bg-first">E-Commerce</h2>
+          <p className="text-gray-600 flex-grow text-sm">
+            This is a modern, responsive e-commerce website built with React, React Router DOM, and Redux Toolkit. It allows users to browse products, filter by category, search for items, view product details, and manage a shopping cart.
           </p>
           <div className="flex justify-end space-x-4 mt-4 text-2xl text-gray-700">
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <a href="https://e-commerce-deploy-0p4z.onrender.com/" target="_blank" rel="noopener noreferrer">
               <CiLink className="hover:text-blue-500 cursor-pointer" />
             </a>
-            <a href="#" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/esrefyapici/e-commerce-deploy" target="_blank" rel="noopener noreferrer">
               <FaGithub className="hover:text-black cursor-pointer" />
             </a>
           </div>
